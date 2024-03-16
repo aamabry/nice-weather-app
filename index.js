386a6e5e364eb80d5f37f8d20b8fe1a849773906
flangoria <audrey.mabry90@gmail.com>
2024-03-16 15:10:08 -0400

    edit current city value

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function search(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-input");
     let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = searchInputElement.ariaValueMax;
+    cityElement.innerHTML = searchInputElement.value;
     apiCity = searchInputElement.value;
     fetchData();
 }
